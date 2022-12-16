bd0ab47d1bb7109ea52dfb1969dc5748c3bd02cd
Fabrice MWANAFUNZI <mwanafunzifabrice@gmail.com>
2022-12-16 12:22:45 +0200

    capstone project

--- scripts/bloglocal.js
+++ scripts/bloglocal.js
@@ -64,6 +64,27 @@ function addImage(imgData){
 
 
 
+let blogimage = document.getElementById("image");
+var image
+
+
+blogimage.addEventListener("change",(e)=>{
+    
+    const img=e.target.files[0];
+
+    const reader=new FileReader();
+
+    reader.readAsDataURL(img);
+    // console.log(reader.readAsDataURL(img))
+  
+    reader.addEventListener("load",()=>{
+        image=reader.result;
+        console.log(image);
+
+    });
+});
+
+
 
 
 
@@ -74,15 +95,15 @@ function addImage(imgData){
 function save() {
     let blogTitle = document.getElementById("blog_title");
     let blogContent = document.getElementById("textarea1");
-    let imageId = document.getElementById("image").files[0];
+    
     // let image = URL.createObjectURL(imageId)
-    console.log(imageId);
+    // console.log(imageId);
    
 
     let data = {
         blogTitle: blogTitle.value,
         blogContent: blogContent.value,
-        image: URL.createObjectURL(imageId)
+        image: image
     };
     details.push(data);
     setData();
